e17f2a0f4b1853157f07001cf2f73f7a0458d8ef
Surya Tetuko <126734340+suryatetukoo@users.noreply.github.com>
2023-03-18 14:08:34 +0700

    Delete page directory

--- page/anggota/create.php
+++
@@ -1,9 +0,0 @@
-<?php
-		$username 		= $_POST['username'];
-		$nama 		= $_POST['nama'];
-		$no_telp 	= $_POST['no_telp'];
-		$password 	= $_POST['password'];
-		$mysqli->query("INSERT INTO dt_user SET username='$username',nama='$nama',no_telp='$no_telp',password='$password',role='Member'");
-		header('location:?page=anggota');
-		exit;
-?>
--- page/anggota/delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$id = $_GET["id"];
-	$mysqli->query("DELETE FROM dt_user WHERE id_user ='$_GET[id]'");
-	header('location:?page=anggota');
-	
- ?>
- 
